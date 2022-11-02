e2df359d0637b5e30e52928dacc55983716ecf85
Giovanni Albano <giovanni.albano1995@libero.it>
2022-11-02 19:12:08 +0100

    #003 handle edit action and graphic fixes

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-list',
@@ -9,11 +10,33 @@ export class TodoListComponent {
 
   @Input() items: Array<string> = [];
 
-
+  public isEdit: boolean = false;
+  public currentElementId: number | null = null;
+  public currentElement: FormControl = new FormControl('');
   constructor() { }
 
   toggle(checked: boolean, i: number) {
     let item: HTMLElement = document.getElementById(`item#${i}`) as HTMLElement;
     checked ? item.classList.add('strikethrough') : item.classList.remove('strikethrough');
   }
+
+  // The function below is called when the user clicks on edit icon
+  editItem(index: number) {
+    this.isEdit = true;
+    this.currentElementId = index;
+    this.currentElement.setValue(this.items[index]);
+  }
+
+  saveNewItem(index: number) {
+    if(this.currentElement?.status !== 'INVALID') {
+       this.items[index] = this.currentElement.value;
+       this.isEdit = false;
+    }
+  }
+
+  onBlur(index: number) {
+    this.isEdit = false;
+    this.currentElement?.status !== 'INVALID' && this.saveNewItem(index);
+  }
 }
+
